ed585a6e85b5716c961fb9d9cb7ade3fb6d4981d
anubhav badak <anubhavbadak@gmail.com>
2021-04-09 19:41:26 +0530

    LOCAL COMMIT TO SEE MERGE CONFLICT

--- c.js
+++ c.js
@@ -1,4 +1,5 @@
 console.log("hey this is c");
 console.log("commit from github -- non merge conflict");
-//MERGE CONFLICT COMMIT
-console.log("commit from github -- merge conflict");
+
+//to show merge conflict i've edited the fle on github and now i'll make changes locally and try to push and will get merge conflict
+console.log("local commit -  merge conflict");
