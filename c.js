708ea9dce39f654efb22a250c96e80247ccb9c6b
anubhavbadak <80116253+anubhavbadak@users.noreply.github.com>
2021-04-09 19:39:51 +0530

    Update c.js
    git commit to see merge conflict

--- c.js
+++ c.js
@@ -2,3 +2,4 @@ console.log("hey this is c");
 console.log("commit from github -- non merge conflict");
 //MERGE CONFLICT COMMIT
 console.log("commit from github -- merge conflict");
+//galti hog thi to duabara change kr rha  hu
