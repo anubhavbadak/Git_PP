0e4436c166bb97fc809bbd6ecd86f58f0ac0435b
anubhav badak <anubhavbadak@gmail.com>
2021-04-09 19:38:26 +0530

    LOCAL COMMIT FOR MERGE CONFLICT

--- a.js
+++ a.js
@@ -1,2 +1,4 @@
 console.log("a");
 console.log("this is a local commit done to study non merge conflict");
+
+//to show merge conflict i've edited the fle on github and now i'll make changes locally and try to push and will get merge conflict
